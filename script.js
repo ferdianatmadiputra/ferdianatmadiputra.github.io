e423ad1145ec90a21b3fd14ff55cfe1dea1467af
ferdianatmadiputra <ferdian.atmadiputra@gmail.com>
2020-12-18 16:13:03 +0700

    Submission

--- script.js
+++ script.js
@@ -1,66 +1,115 @@
 
-let pickerA = document.getElementById("color-a"),
-pickerB = document.getElementById("color-b");
+let pickerA = document.getElementById("color-a");
+let pickerB = document.getElementById("color-b");
 
-const colorA = pickerA.value,
-colorB = pickerB.value;
+let hexTextA = document.getElementById("hexTextA");
+let hexTextB = document.getElementById("hexTextB");
 
-let hexTextA = document.getElementById("hexTextA"),
-hexTextB = document.getElementById("hexTextB");
+hexTextA.innerHTML = pickerA.value;
+hexTextB.innerHTML = pickerB.value;
 
-let preview = document.getElementById("preview"),
-see = document.getElementById("see");
+let wrapperColorA = document.getElementById("color-wrap-a");
+let wrapperColorB = document.getElementById("color-wrap-b");
 
-let direction = document.getElementsByClassName("direction");
+let rotate = document.getElementById("rotate");
+let degree = 0;
 
-let generate = document.getElementById("generate");
+let copyCss = document.getElementById("copy-css");
+let buttonCopy = document.getElementById("btn-copy");
 
-hexTextA.innerHTML = colorA;
-hexTextB.innerHTML = colorB;
 
-let code;
+let randomizer = document.getElementById("randomizer");
 
-let percentageA = document.getElementById("range-a"),
-percentageB = document.getElementById("range-b");
+let code = `background: linear-gradient(${rotate.value}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
 
-for (let i = 0; i < direction.length; i++ ) {
-
-    direction[i].addEventListener("click", (event) => {
-        document.getElementById("direction-input").innerText = `${direction[i].value}`;
-        document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
-    });
-
-};
-
-pickerA.addEventListener("input", (event) => {
+//refreshA refreshB for updating all neccesary objects to make the exp more seamless
+let refreshA = function(){
+    wrapperColorA.style.backgroundColor = pickerA.value;
     hexTextA.innerHTML = pickerA.value;
-    document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
-}, false);
+    document.body.style = `background: linear-gradient(${rotate.value}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
+}
 
-pickerB.addEventListener("input", (event) => {
+let refreshB = function() {
+    wrapperColorB.style.backgroundColor = pickerB.value;
     hexTextB.innerHTML = pickerB.value;
-    document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+    document.body.style = `background: linear-gradient(${rotate.value}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
+}
+
+// event for color picker a
+pickerA.addEventListener("input", refreshA);
+pickerA.addEventListener("change", refreshA);
+
+//event for color picker b
+pickerB.addEventListener("input", refreshB);
+pickerB.addEventListener("change", refreshB);
+
+//rotate gradient linear
+rotate.addEventListener("click", (event) => {
+    degree += 45;
+    if (degree === 360){
+        degree = 0;
+    }
+    rotate.value = `${degree}deg`;
+    document.body.style = `background: linear-gradient(${rotate.value}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
 }, false);
 
-percentageA.addEventListener("input", (event) => {
-    document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
-});
+//copy css code
 
-percentageB.addEventListener("input", (event) => {
-    document.body.style = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
-});
-
-
-
-generate.addEventListener("click", (event) => {
-generate.innerText = "Copied! 🥳";
-setTimeout(function(){ generate.innerText = "Copy Code"; }, 1300);
-let code = `background: linear-gradient(to ${document.getElementById("direction-input").textContent}, ${hexTextA.innerHTML} ${parseInt(percentageA.value,10)}%, ${hexTextB.innerHTML} ${parseInt(percentageB.value,10)}%);`;
+buttonCopy.addEventListener("click", (event) => {
+    copyCss.innerHTML = "Copied!";
+    setTimeout(function(){ copyCss.innerHTML = "Copy Code"; }, 1300);
+    code = `background: linear-gradient(${rotate.value}, ${hexTextA.innerHTML} 0%, ${hexTextB.innerHTML} 100%)`;
     let codeToCopy = document.createElement('textarea');
     codeToCopy.value = code;
     document.body.appendChild(codeToCopy);
     codeToCopy.select();
     document.execCommand('copy');
     document.body.removeChild(codeToCopy);
-  
-})
+}, false);
+
+//copy hex value
+hexTextA.addEventListener("click", (event) => {
+    let temp = hexTextA.innerHTML;
+    hexTextA.innerHTML = "Copied!";
+    setTimeout(function(){ hexTextA.innerHTML = temp; }, 1300);
+    let codeToCopy = document.createElement('textarea');
+    codeToCopy.value = hexTextA.innerHTML;
+    document.body.appendChild(codeToCopy);
+    codeToCopy.select();
+    document.execCommand('copy');
+    document.body.removeChild(codeToCopy);
+}, false);
+
+hexTextB.addEventListener("click", (event) => {
+    let temp = hexTextB.innerHTML;
+    hexTextB.innerHTML = "Copied!";
+    setTimeout(function(){ hexTextB.innerHTML = temp; }, 1300);
+    let codeToCopy = document.createElement('textarea');
+    codeToCopy.value = hexTextB.innerHTML;
+    document.body.appendChild(codeToCopy);
+    codeToCopy.select();
+    document.execCommand('copy');
+    document.body.removeChild(codeToCopy);
+}, false);
+
+//generate random color
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let randomhex = '#';
+    for (var i = 0; i < 6; i++) {
+      randomhex += letters[Math.floor(Math.random() * 16)];
+    }
+    return randomhex;
+  }
+
+randomizer.addEventListener("click", (event) => {
+    let randomColorA = getRandomColor();
+    let randomColorB = getRandomColor();
+
+    pickerA.value = randomColorA;
+    pickerB.value = randomColorB;
+    refreshA();
+    refreshB();
+}, false);
+
+//planned to also build radial gradient feature, but I cant make it on time I guess :(
